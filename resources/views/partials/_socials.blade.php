cc8ae2c387e910600a00682b76f107b5a582e5fe
Amir Reza Mehbakhsh <theamirrezam75@gmail.com>
2022-10-26 20:32:15 +0330

    Add linkedin, github and stackoverflow links

--- resources/views/partials/_socials.blade.php
+++ resources/views/partials/_socials.blade.php
@@ -1,5 +1,8 @@
 <ul class="social-icons" style="{{ $styles ?? ''}}">
-    <li><a href="https://www.instagram.com/amirrezam75/" class="tooltips tooltipped facebook" data-position="top" data-delay="50" data-tooltip="Instagram"><i class="fa fa-instagram"></i></a></li>
+    <li><a href="https://stackoverflow.com/users/5827477/amirrezam75" class="tooltips tooltipped dribbble" data-position="top" data-delay="50" data-tooltip="Stack Overflow"><i class="fa fa-stack-overflow"></i></a></li>
     <li><a href="https://t.me/TheAmirRezaM75" class="tooltips tooltipped linkedin" data-position="top" data-delay="50" data-tooltip="Telegram"><i class="fa fa-telegram"></i></a></li>
+    <li><a href="https://www.instagram.com/amirrezam75/" class="tooltips tooltipped facebook" data-position="top" data-delay="50" data-tooltip="Instagram"><i class="fa fa-instagram"></i></a></li>
     <li><a href="https://twitter.com/AmirRezaM75" class="tooltips tooltipped twitter" data-position="top" data-delay="50" data-tooltip="Twitter"><i class="fa fa-twitter"></i></a></li>
+    <li><a href="https://www.linkedin.com/in/amirrezam75/" class="tooltips tooltipped linkedin" data-position="top" data-delay="50" data-tooltip="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+    <li><a href="https://github.com/AmirRezaM75" class="tooltips tooltipped" data-position="top" data-delay="50" data-tooltip="Github"><i class="fa fa-github"></i></a></li>
 </ul> <!-- ./social icons end -->
